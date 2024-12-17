a814c5a64d159951ac5c5f53ac962a8f5671d290
Robert Nemeth <104597057+Robertsnemeth@users.noreply.github.com>
2024-12-16 23:00:47 -0800

    set up vercel postgres and drizzle studio, set up db and presisted data

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,24 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { usersTable } from '../db/schema';
+  
+export const db = drizzle(process.env.DATABASE_URL!);
 
-export  const db = drizzle(process.env.DATABASE_URL!);
+type User = {
+  name: string;
+  age: number;
+  email: string;
+}
+
+
+export async function addUser(user: User) {
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+}
+
+export async function getUsers() {
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  return users;
+}
+
